ce479fce663af570cc77ad3810f5289daba197b5
phosgene89 <34409398+phosgene89@users.noreply.github.com>
2019-08-20 04:52:15 +0930

    Create model_performance_analysis.py

---
+++ mlscripts/model_performance_analysis.py
@@ -0,0 +1,33 @@
+from sklearn.metrics import confusion_matrix
+import sklearn.metrics as metrics
+
+# Confusion matrix, sensitivity and specificity
+y_prob = model.predict(X_test) 
+y_classes = y_prob
+y_classes[y_classes<0.5]=0
+y_classes[y_classes!=0]=1
+
+cm1 = confusion_matrix(y_test, y_classes)
+print("Confusion matrix:")
+print(cm1)
+
+sensitivity1 = cm1[0,0]/(cm1[0,0]+cm1[0,1])
+print('Sensitivity : ', sensitivity1 )
+
+specificity1 = cm1[1,1]/(cm1[1,0]+cm1[1,1])
+print('Specificity : ', specificity1)
+
+# ROC curve and AUC
+scores = y_prob
+fpr, tpr, thresholds = metrics.roc_curve(y_test, scores, pos_label=1)
+roc_auc = metrics.auc(fpr, tpr)
+
+plt.title('Receiver Operating Characteristic')
+plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+plt.legend(loc = 'lower right')
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0, 1])
+plt.ylim([0, 1])
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.show()
